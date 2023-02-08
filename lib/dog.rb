fcc8afc8ddc3d5a2b56e0167b6ac2b0251c0ac1d
T-erry <terrywachuka10@gmail.com>
2023-02-09 01:43:01 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
-
+    attr_accessor :name
+    attr_accessor :breed
+    def name=(name)
+        @name = name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
 end
+dog1 = Dog.new
+dog1.name = "Fido"
+dog1.breed = "Beagle"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    attr_accessor :name
+    attr_accessor :job
+    def name=(name)
+        @name = name
+    end
+    # def name
+    #     @name
+    # end
+    def job=(job)
+        @job = job
+    end
 
 end
+person1 = Person.new
+ person1.name = "Kanye"
+ person1.job = "Singer"
+ puts person1
+ 
